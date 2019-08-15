1ff049e104152a197c87731216eea51bfed9c036
TexFromHell <nivedasteam@gmail.com>
2019-08-15 04:23:48 +0100

    navigation still in progress;

--- backend/server.js
+++ backend/server.js
@@ -5,49 +5,49 @@ const crypto = require('crypto')
 const mime = require('mime')
 const app = express()
 const port = 3000
-const multer  = require('multer')
+const multer = require('multer')
 const csv = require('csv-parser');
 
 //stores the uploaded files on the server
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  },
-  filename: function (req, file, cb) {
-    crypto.pseudoRandomBytes(16, function (err, raw) {
-      cb(null, raw.toString('hex') + Date.now() + '.' + file.originalname.split('.')[1]);
-    });
-  }
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+            cb(null, raw.toString('hex') + Date.now() + '.' + file.originalname.split('.')[1]);
+        });
+    }
 });
 
 
 app.get('/', (req, res) => {
-	res.json({message:'yo skrzek'})
+    res.json({message: 'yo skrzek'})
 })
 
-var uploader = multer({ storage: storage });
+var uploader = multer({storage: storage});
 
 app.post('/uploadFile', uploader.single('uploadedFile'), (req, res) => {
-  let results = []
-  fs.createReadStream(req.file.originalname)
-    .pipe(csv())
-    .on('data', (row) => {
-      console.log(row.Longitude, 'long');
-      console.log(row.Latitude, 'lat');
-      console.log(row['Crime type'], 'crime')
+    let results = []
+    fs.createReadStream(req.file.originalname)
+        .pipe(csv())
+        .on('data', (row) => {
+            console.log(row.Longitude, 'long');
+            console.log(row.Latitude, 'lat');
+            console.log(row['Crime type'], 'crime')
 
-      let data = {
-        long:row.Longitude,
-        lat:row.Latitude,
-        crime:row['Crime type'],
-      }
+            let data = {
+                long: row.Longitude,
+                lat: row.Latitude,
+                crime: row['Crime type'],
+            }
 
-      results.push(data)
-    })
-    .on('end', () => {
-      console.log('CSV file successfully processed');
-      res.json({results:results})
-    });
+            results.push(data)
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            res.json({results: results})
+        });
 })
 
 
--- js/main.js
+++ js/main.js
@@ -1,143 +1,144 @@
 window.addEventListener('load', () => {
 
-  const submitButton = window.submit;
-  let map = initMap()
+    const submitButton = window.submit;
+    let map = initMap()
 
-  submitButton.addEventListener('click', () => {
-    let selectedFile = window.uploadedFile.files[0]
+    submitButton.addEventListener('click', () => {
+        let selectedFile = window.uploadedFile.files[0]
 
-    console.log(selectedFile.path)
+        console.log(selectedFile.path)
 
-    let data = new FormData();
+        let data = new FormData();
         data.append('uploadedFile', selectedFile);
 
         uploadFile('/uploadFile', data).then((response) => {
-          console.log(response)
-          response.results.forEach((item) => {
-            let markerLoc = new google.maps.LatLng(item.lat,item.long);
-            console.log(item)
+            console.log(response)
+            response.results.forEach((item) => {
+                let markerLoc = new google.maps.LatLng(item.lat, item.long);
+                console.log(item)
 
-          const crimeIcons = {
-            'Anti-social behaviour': {
-              url: 'icons/anti-social.png'
-            },
-            'Other crime': {
-              url:'icons/other-crime.png'
-            },
-            'Vehicle crime': {
-              url: 'icons/vehicle.png'
-            },
-            'Violence and sexual offences': {
-              url: 'icons/rape.png'
-            },
-            'Public order': {
-              url: 'icons/public-order.png'
-            },
-            'Other theft': {
-              url: 'icons/other-theft.png'
-            },
-            'Criminal damage and arson': {
-              url: 'icons/fire.png'
-            },
-            'Drugs': {
-              url: 'icons/drug.png'
-            },
-            'Possession of weapons': {
-              url: 'icons/shooting.png'
-            },
-            'Theft from the person': {
-              url: 'icons/theft.png'
-            },
-            'Robbery': {
-              url: 'icons/robbery.png'
-            },
-            'Shoplifting': {
-              url: 'icons/shop.png'
-            },
-            'Bicycle theft': {
-              url: 'icons/bicycle.png'
-            },
-            'Burglary': {
-              url: 'icons/burglary.png'
-            }
-          }
+                const crimeIcons = {
+                    'Anti-social behaviour': {
+                        url: 'icons/anti-social.png'
+                    },
+                    'Other crime': {
+                        url: 'icons/other-crime.png'
+                    },
+                    'Vehicle crime': {
+                        url: 'icons/vehicle.png'
+                    },
+                    'Violence and sexual offences': {
+                        url: 'icons/rape.png'
+                    },
+                    'Public order': {
+                        url: 'icons/public-order.png'
+                    },
+                    'Other theft': {
+                        url: 'icons/other-theft.png'
+                    },
+                    'Criminal damage and arson': {
+                        url: 'icons/fire.png'
+                    },
+                    'Drugs': {
+                        url: 'icons/drug.png'
+                    },
+                    'Possession of weapons': {
+                        url: 'icons/shooting.png'
+                    },
+                    'Theft from the person': {
+                        url: 'icons/theft.png'
+                    },
+                    'Robbery': {
+                        url: 'icons/robbery.png'
+                    },
+                    'Shoplifting': {
+                        url: 'icons/shop.png'
+                    },
+                    'Bicycle theft': {
+                        url: 'icons/bicycle.png'
+                    },
+                    'Burglary': {
+                        url: 'icons/burglary.png'
+                    }
+                }
 
-            var marker = new google.maps.Marker({
-              position: markerLoc,
-              title: item.crime,
-              icon: crimeIcons[item.crime]
-            });
-            marker.setMap(map);
-            map.panTo(new google.maps.LatLng(item.lat, item.long))
-          })
+                var marker = new google.maps.Marker({
+                    position: markerLoc,
+                    title: item.crime,
+                    icon: crimeIcons[item.crime]
+                });
+                marker.setMap(map);
+                map.panTo(new google.maps.LatLng(item.lat, item.long))
+            })
         })
-  })
-
-  const menuWindows = Array.from(document.querySelectorAll('.menuWindow'))
-  const tabs = Array.from(document.querySelectorAll('.tabItem'))
-  const mainMenu = window.mainMenu
-  const back = window.closeRightNav
-
-  mainMenu.addEventListener('click', () => {
-    window.tabs.classList.remove('hidden') // show the tabs again
-
-    menuWindows.map((window) => {
-      window.classList.add('hidden') //hide all current opened windows
     })
 
-  })
+    const menuWindows = Array.from(document.querySelectorAll('.menuWindow'))
+    const tabs = Array.from(document.querySelectorAll('.tabItem'))
+    const mainMenu = window.mainMenu
+    const back = window.closeRightNav
 
-  tabs.map((tab) => {
-      tab.addEventListener('click', () => {
-        const target = tab.getAttribute('data-target')
-        const toShow = document.getElementById(target)
+    mainMenu.addEventListener('click', () => {
+        window.tabs.classList.remove('hidden') // show the tabs again
 
-        if (toShow) {
-          toShow.classList.remove('hidden')
-        }
-        window.tabs.classList.add('hidden')
-      })
-  })
+        menuWindows.map((window) => {
+            window.classList.add('hidden') //hide all current opened windows
+        })
+
+    })
+
+    tabs.map((tab) => {
+        tab.addEventListener('click', () => {
+            const target = tab.getAttribute('data-target')
+            const toShow = document.getElementById(target)
+
+            if (toShow) {
+                toShow.classList.remove('hidden')
+                window.back.classList.remove('hidden')
+            }
+            window.tabs.classList.add('hidden')
+        })
+    })
 })
 
 
 function uploadFile(url, data) {
-  return new Promise((resolve, reject) => {
-    fetch('http://localhost:3000/uploadFile', {
-      method: 'POST',
-      body: data,
-    }).then(function (res) {
-      return res.json()
-    }).then((data) => {
-      resolve(data)
-    }).catch(error => {
-      reject(error)
-    });
-  })
+    return new Promise((resolve, reject) => {
+        fetch('http://localhost:3000/uploadFile', {
+            method: 'POST',
+            body: data,
+        }).then(function (res) {
+            return res.json()
+        }).then((data) => {
+            resolve(data)
+        }).catch(error => {
+            reject(error)
+        });
+    })
 }
 
 
 function initMap() {
 
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.5074, lng:0.1078},
-    zoom: 10,
-    disableDefaultUI: false
-  });
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 51.5074, lng: 0.1078},
+        zoom: 10,
+        disableDefaultUI: false
+    });
 
-  return map
+    return map
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var leftBar = document.getElementById('slide-out-left')
-  var instance1 = M.Sidenav.init(leftBar);
-  var rightBar = document.getElementById('slide-out-right')
-  var instance2 = M.Sidenav.init(rightBar, {edge:'right'});
+document.addEventListener('DOMContentLoaded', function () {
+    var leftBar = document.getElementById('slide-out-left')
+    var instance1 = M.Sidenav.init(leftBar);
+    var rightBar = document.getElementById('slide-out-right')
+    var instance2 = M.Sidenav.init(rightBar, {edge: 'right'});
 
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
 });
